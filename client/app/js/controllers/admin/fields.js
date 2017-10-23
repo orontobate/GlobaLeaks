b18c26ae2f88b2a2abb569c8fb341b954c4050ef
nskelsey <nskelsey@gmail.com>
2017-10-23 10:24:08 +0200

    Fix eslint detected warnings in admin controllers

--- client/app/js/controllers/admin/fields.js
+++ client/app/js/controllers/admin/fields.js
@@ -86,7 +86,7 @@ GLClient.controller('AdminFieldEditorCtrl', ['$scope', '$uibModal', 'Utils',
     $scope.moveOptionUp = function(idx) { swapOption(idx, -1); };
     $scope.moveOptionDown = function(idx) { swapOption(idx, 1); };
 
-    swapOption = function(index, n) {
+    function swapOption(index, n) {
       var target = index + n;
       if (target < 0 || target >= $scope.field.options.length) {
         return;
@@ -95,7 +95,7 @@ GLClient.controller('AdminFieldEditorCtrl', ['$scope', '$uibModal', 'Utils',
       var b = $scope.field.options[index];
       $scope.field.options[target] = b;
       $scope.field.options[index] = a;
-    };
+    }
 
     $scope.delOption = function(option) {
       $scope.field.options.splice($scope.field.options.indexOf(option), 1);
